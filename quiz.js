2d43e152fcbfb250338d0d47144cddb7a13ec6fd
Musabe Yvan <yvanzomusa@gmail.com>
2025-08-04 19:03:34 +0200

    Added a JS file

---
+++ quiz.js
@@ -0,0 +1,17 @@
+function checkAnswer () {
+  const correctAnswer = '4';
+  const selectedOption = document.querySelector('input[name="checked"]: checked');
+
+  if (!selectedOption) return;
+  const userAnswer = selectedOption.value;
+  const feedback = document.getElementById('feedback');
+  if (userAnswer == correctAnswer) {
+    feedback.textContent = 'Correct! Well done.';
+  } else {
+    feedback.textContent = "That's incorrect. Try again!"
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("submit-answer").addEventListener("click", checkAnswer);
+})
